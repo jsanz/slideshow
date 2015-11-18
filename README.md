ff3fd8197125979ccee5811ecacf4c02e12f73bf
Jorge Sanz <jsanz@cartodb.com>
2015-11-18 10:49:29 +0100

    parametrized tables

---
+++ README.md
@@ -0,0 +1,17 @@
+iFrame Slideshow
+=======================
+
+Create a set of slides that render iframes and loops over them automatically.
+
+Call http://jsanz.github.io/slideshow/?cartodb=user/table where
+
+- `user` is your user name at [CartoDB](http://cartodb.com)
+- `table` is a table on your CartoDB account with two columns: `url` and `title` to feed your slides. You can leave it blank if the table is named `slideshow`.
+
+Things on the to-do list:
+
+- Allow customization of the slideshow (autoloop, theming, time between slides, etc)
+- I'll explore other options like passing a custom url or a CSV when I have more time.
+
+Enjoy!
+Jorge
---
+++ js/slideshow.js
@@ -0,0 +1,61 @@
+/* http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html*/
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$(document).ready(function(){
+	var template = $('#section').html();
+	var slides = $('.slides');
+
+	var cartodb_table = getUrlParameter('cartodb') || 'jsanz/slideshow';
+
+	var cdb_comps = cartodb_table.split('/');
+	var user = cdb_comps[0];
+	var table = cdb_comps[1] || 'slideshow';
+
+	// TODO support also CSV by url
+
+	new cartodb.SQL({'user': user})
+	.execute('select url,title from ' + table + ' limit 2')
+	.done(function(data){
+		$('h2.loading').remove();
+		data.rows.forEach(function(d){
+			slides.append(
+				template.replace('{{title}}',d.title).replace(/{{url}}/gi,d.url)
+			);
+		})
+
+		// Full list of configuration options available at:
+		// https://github.com/hakimel/reveal.js#configuration
+		Reveal.initialize({
+			controls: true,
+			progress: true,
+			history: true,
+			center: true,
+			loop: true,
+			autoSlide: 15000,
+			transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+			// Optional reveal.js plugins
+			dependencies: [
+				{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+				{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+				{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+				{ src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+				{ src: 'plugin/zoom-js/zoom.js', async: true },
+				{ src: 'plugin/notes/notes.js', async: true }
+			]
+		});
+	});
+});
